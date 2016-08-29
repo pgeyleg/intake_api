56c39bbc00bd74f9a0fdd5b10c4db7fae0d56feb
Nate Jones and Nora Lin <pair+nate+nora@casecommons.org>
2016-08-29 16:23:41 -0400

    Nest referral address attributes
    Use nested attributes to persist data
    
    [#127201331]

--- app/controllers/api/v1/referrals_controller.rb
+++ app/controllers/api/v1/referrals_controller.rb
@@ -4,26 +4,38 @@ module Api
   module V1
     class ReferralsController < ApplicationController # :nodoc:
       def create
-        referral = Referral.create(referral_params)
-        render json: referral, status: :created
+        @referral = Referral.new(referral_params)
+        @referral.build_referral_address
+        @referral.referral_address.build_address
+        @referral.save!
+        render json: referral_json, status: :created
       end
 
       def show
-        referral = Referral.find(params[:id])
-        render json: referral, status: :ok
+        @referral = Referral.find(params[:id])
+        render json: referral_json, status: :ok
       end
 
       def update
-        referral = Referral.find(params[:id])
-        referral.update!(referral_params)
-        render json: referral, status: :ok
+        @referral = Referral.find(params[:id])
+        @referral.update!(referral_params)
+        render json: referral_json, status: :ok
       end
 
       private
 
+      def referral_json
+        @referral.as_json(
+          include: {
+            referral_address: {
+              include: :address, only: [:id, :address]
+            }
+          }
+        )
+      end
+
       def referral_params
         params.permit(
-          :city,
           :ended_at,
           :id,
           :incident_date,
@@ -32,9 +44,16 @@ module Api
           :name,
           :reference,
           :started_at,
-          :state,
-          :street_address,
-          :zip
+          referral_address_attributes: [
+            :id,
+            address_attributes: [
+              :id,
+              :street_address,
+              :city,
+              :state,
+              :zip
+            ]
+          ]
         )
       end
     end
--- app/models/referral.rb
+++ app/models/referral.rb
@@ -3,4 +3,6 @@
 # Referral model which represents the referral
 class Referral < ActiveRecord::Base
   has_one :referral_address, inverse_of: :referral
+
+  accepts_nested_attributes_for :referral_address, allow_destroy: true
 end
