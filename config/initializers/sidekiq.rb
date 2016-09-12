2a05de5afd8405cec56655300d91335c53630a25
Suman Gurung <suman@casecommons.org>
2016-09-12 10:45:32 -0400

    Use redis db '0' for development and '1' for test
    Redis allows multiple databases on the same instance and we can use this
    set up a different db for testing, so that there is no collision between test data
    and development data.
    
    [#128067971]

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-sidekiq_config = { url: ENV['REDIS_URL'] }.freeze
+db = Rails.env.test? ? 1 : 0
+sidekiq_config = { url: "#{ENV['REDIS_URL']}/#{db}" }.freeze
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
