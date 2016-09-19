7ae283635ba73830672ac689a61f735dfd172f91
Suman Gurung + Nora Lin <devs+sg+nl@casecommons.org>
2016-09-19 13:16:24 -0400

    Set up test redis db from docker-compose.yml
    [#128067971]

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 db = Rails.env.test? ? 1 : 0
-sidekiq_config = { url: "#{ENV['REDIS_URL']}/#{db}" }.freeze
+redis_url = if Rails.env.test?
+              ENV['TEST_REDIS_DATABASE']
+            else
+              ENV['REDIS_DATABASE']
+            end
+sidekiq_config = { url: redis_url }.freeze
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
