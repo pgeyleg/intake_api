b64eec5a7b4558ad2684470831d9d6eac96c9b60
Suman Gurung + Nora Lin <devs+sg+nl@casecommons.org>
2016-09-20 13:37:17 -0400

    Remove dead code and don't freeze sidekia config because sidekiq modifies it

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-db = Rails.env.test? ? 1 : 0
 redis_url = if Rails.env.test?
               ENV['TEST_REDIS_DATABASE']
             else
               ENV['REDIS_DATABASE']
             end
-sidekiq_config = { url: redis_url }.freeze
+sidekiq_config = { url: redis_url }
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
