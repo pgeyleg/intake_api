311a384c4189e71a1a97aac6f7b0c46f36cf1202
Sri Harsha Poosa and Thomas Ramirez <pair+Sri.Poosa+Thomas.Ramirez@OSI.ca.gov>
2016-10-17 16:02:29 -0700

    Update per pull request comments
    [#132333073]

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+DOCKER_ORGANIZATION="casecommons"
+DOCKER_REPO="intake_api_prototype"
+SHA=$(git rev-parse --short HEAD)
+
 git checkout master
 git pull --rebase
-docker build -f Dockerfile.production -t casecommons/intake_api_prototype:$(git rev-parse --short HEAD) .
-docker push casecommons/intake_api_prototype:$(git rev-parse --short HEAD)
+docker build -f Dockerfile.production -t $DOCKER_ORGANIZATION/$DOCKER_REPO:$SHA .
+docker push casecommons/intake_api_prototype:$SHA
