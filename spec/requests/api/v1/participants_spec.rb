655f69c95458bf3f493a4f28eedf33dbcb4d5a01
Nate Jones and Sri Harsha Poosa <pair+Nate.Jones+Sri.Poosa@OSI.ca.gov>
2016-11-01 16:47:48 -0700

    Add participants create request spec
    [#133475507]

---
+++ spec/requests/api/v1/participants_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe 'Participants API' do
+  describe 'POST /api/v1/participants' do
+    it 'creates a participant' do
+      participant_params = {
+        person_id: nil,
+        screening_id: nil,
+        first_name: 'Walter',
+        last_name: 'White',
+        gender: 'female',
+        date_of_birth: '1990-03-30',
+        ssn: '345-12-2345',
+      }
+
+      post '/api/v1/participants', params: participant_params
+
+      expect(response.status).to eq(201)
+      body = JSON.parse(response.body).with_indifferent_access
+      expect(body).to include(
+        person_id: nil,
+        screening_id: nil,
+        first_name: 'Walter',
+        last_name: 'White',
+        gender: 'female',
+        date_of_birth: '1990-03-30',
+        ssn: '345-12-2345',
+      )
+      expect(body['id']).to_not eq nil
+    end
+  end
+end
