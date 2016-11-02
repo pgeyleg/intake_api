999d7c4a7f32364a3bf704f4fe560c980dc3a953
Nate Jones and Sri Harsha Poosa <pair+Nate.Jones+Sri.Poosa@OSI.ca.gov>
2016-11-02 11:06:10 -0700

    Remove unnecessary instance variables
    [#133475507]

--- app/controllers/api/v1/screenings_controller.rb
+++ app/controllers/api/v1/screenings_controller.rb
@@ -4,24 +4,24 @@ module Api
   module V1
     class ScreeningsController < ApplicationController # :nodoc:
       def create
-        @screening = Screening.new(screening_params)
-        @screening.build_screening_address
-        @screening.screening_address.build_address
-        @screening.save!
-        render json: ScreeningSerializer.new(@screening), status: :created
+        screening = Screening.new(screening_params)
+        screening.build_screening_address
+        screening.screening_address.build_address
+        screening.save!
+        render json: ScreeningSerializer.new(screening), status: :created
       end
 
       def show
-        @screening = Screening.find(screening_params[:id])
-        render json: ScreeningSerializer.new(@screening), status: :ok
+        screening = Screening.find(screening_params[:id])
+        render json: ScreeningSerializer.new(screening), status: :ok
       end
 
       def update
-        @screening = Screening.find(screening_params[:id])
-        @screening.assign_attributes(screening_params)
-        @screening.address.assign_attributes(address_params)
-        @screening.save!
-        render json: ScreeningSerializer.new(@screening), status: :ok
+        screening = Screening.find(screening_params[:id])
+        screening.assign_attributes(screening_params)
+        screening.address.assign_attributes(address_params)
+        screening.save!
+        render json: ScreeningSerializer.new(screening), status: :ok
       end
 
       private
