0b13f05543b4097c5822c927455a761acb8b5abf
Nate Jones and Sri Harsha Poosa <pair+Nate.Jones+Sri.Poosa@OSI.ca.gov>
2016-11-02 11:08:20 -0700

    Cleanup person serializer attributes
    [#133475507]

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 class PersonSerializer < ActiveModel::Serializer # :nodoc:
   has_one :address
-  attributes :id, :first_name, :last_name, :gender, :ssn, :date_of_birth
+  attributes :id,
+    :first_name,
+    :last_name,
+    :gender,
+    :ssn,
+    :date_of_birth
 end
