068985831a6abf87955e84f780ca4e76f2dcad7d
Nate Jones and Sri Harsha Poosa <pair+Nate.Jones+Sri.Poosa@OSI.ca.gov>
2016-11-03 11:11:45 -0700

    Remove screening people
    [#133475507]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101235059) do
+ActiveRecord::Schema.define(version: 20161102190951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,15 +64,6 @@ ActiveRecord::Schema.define(version: 20161101235059) do
     t.datetime "updated_at",   null: false
     t.index ["address_id"], name: "index_screening_addresses_on_address_id", using: :btree
     t.index ["screening_id"], name: "index_screening_addresses_on_screening_id", using: :btree
-  end
-
-  create_table "screening_people", force: :cascade do |t|
-    t.integer  "screening_id"
-    t.integer  "person_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["person_id"], name: "index_screening_people_on_person_id", using: :btree
-    t.index ["screening_id"], name: "index_screening_people_on_screening_id", using: :btree
   end
 
   create_table "screenings", force: :cascade do |t|
