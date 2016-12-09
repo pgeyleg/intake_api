2c58ec9411e2ff885edb0a385e632c9381c3f483
Siva Mukunda <Siva.Mukunda@OSI.ca.gov>
2016-12-09 09:06:53 -0800

    Add address type to person data model
    [#133272833]

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 # Address model which represents the physical address of a person
 class Address < ActiveRecord::Base
+  self.inheritance_column = nil
   belongs_to :person, optional: true
+  ADDRESS_TYPES = %w(Home School Work Placement Homeless Other).freeze
+  validates :type,
+    inclusion: { in: ADDRESS_TYPES, allow_nil: true }
 end
