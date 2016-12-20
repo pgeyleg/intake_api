97fb844c9b57f7ae0b7f9112bdccba1ee483554b
Siva Mukunda <Siva.Mukunda@OSI.ca.gov>
2016-12-20 14:29:52 -0800

    Add addresses to person data model
    [#135438679]

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 # Person model which represents a real world person
 class Person < ActiveRecord::Base
-  has_one :person_address, inverse_of: :person
-  has_one :address, through: :person_address
+  has_many :person_addresses, inverse_of: :person
+  has_many :addresses, through: :person_addresses
   has_many :person_phone_numbers, inverse_of: :person
   has_many :phone_numbers, through: :person_phone_numbers
 
-  accepts_nested_attributes_for :address
 
   after_commit :reindex
 
