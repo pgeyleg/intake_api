430180b339ffaf106b35b70d143189163d0cd9d0
Siva Mukunda <Siva.Mukunda@OSI.ca.gov>
2017-02-15 13:29:18 -0800

    Create a join table participant_address in our model
    [#138164459]

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,6 +3,8 @@
 # Participant model which represents a real world
 # person on a screening
 class Participant < ActiveRecord::Base
+  has_many :participant_addresses, inverse_of: :participant
+  has_many :addresses, through: :participant_addresses
   belongs_to :screening
   belongs_to :person
 end
