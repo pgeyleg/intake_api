fda40c07471c86b85584f17bbbaaeb7b7ba141b5
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-05-09 08:26:41 -0700

    Use FactoryGirl in participant serializer spec
    [#refactor]

--- spec/serializers/referral_participant_serializer_spec.rb
+++ spec/serializers/referral_participant_serializer_spec.rb
@@ -4,56 +4,35 @@ require 'rails_helper'
 
 describe ReferralParticipantSerializer do
   describe 'as_json' do
-    let(:person) { Person.create }
-    let(:screening) { Screening.create }
     let(:participant) do
-      Participant.new(
-        first_name: 'Robert',
-        last_name: 'Smith',
-        gender: 'male',
-        ssn: '111223333',
-        date_of_birth: Date.parse('1955-01-31'),
-        person: person,
-        screening: screening,
+      FactoryGirl.create(
+        :participant,
+        :with_address,
+        person: FactoryGirl.build(:person),
         roles: ['Victim']
       )
-    end
-
-    before do
-      participant.addresses.build(
-        street_address: '1840 Broad rd',
-        state: 'CA',
-        city: 'sacramento',
-        zip: '78495',
-        type: 'Work'
-      )
-      participant.phone_numbers.build(
-        number: '1234567891',
-        type: 'Work'
-      )
-      participant.save!
     end
 
     it 'returns the attributes of a participant as a hash' do
       expect(described_class.new(participant).as_json).to eq(
         id: participant.id,
-        first_name: 'Robert',
-        last_name: 'Smith',
-        gender: 'male',
-        ssn: '111223333',
-        date_of_birth: Date.parse('1955-01-31'),
-        person_id: person.id,
-        screening_id: screening.id,
+        first_name: participant.first_name,
+        last_name: participant.last_name,
+        gender: participant.gender,
+        ssn: participant.ssn,
+        date_of_birth: participant.date_of_birth,
+        person_id: participant.person_id,
+        screening_id: participant.screening_id,
         addresses: [
           {
-            street_address: '1840 Broad rd',
-            state: 'CA',
-            city: 'sacramento',
-            zip: '78495',
-            type: 'Work'
+            street_address: participant.addresses.first.street_address,
+            state: participant.addresses.first.state,
+            city: participant.addresses.first.city,
+            zip: participant.addresses.first.zip,
+            type: participant.addresses.first.type
           }
         ],
-        roles: ['Victim']
+        roles: participant.roles
       )
     end
   end
