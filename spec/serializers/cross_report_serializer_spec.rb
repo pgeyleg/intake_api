fd8cb10f6cf538c6c42ccf56cb9be024000af1d4
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-05-09 16:43:52 -0700

    Use FactoryGirl in cross report spec
    [#143996393]

--- spec/serializers/cross_report_serializer_spec.rb
+++ spec/serializers/cross_report_serializer_spec.rb
@@ -4,18 +4,12 @@ require 'rails_helper'
 
 describe CrossReportSerializer do
   describe 'as_json' do
-    let(:cross_report) do
-      FactoryGirl.build(
-        :cross_report,
-        agency_type: 'District attorney',
-        agency_name: 'Sacramento attorney'
-      )
-    end
+    let(:cross_report) { FactoryGirl.build(:cross_report) }
 
     it 'returns the attributes of a cross report on a hash' do
       expect(described_class.new(cross_report).as_json).to eq(
-        agency_type: 'District attorney',
-        agency_name: 'Sacramento attorney'
+        agency_type: cross_report.agency_type,
+        agency_name: cross_report.agency_name
       )
     end
   end
